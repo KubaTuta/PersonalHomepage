13590a732b954fdb42ef4efd0899189e9924219d
Kuba Tuta <tuta.jakub@gmail.com>
2023-02-26 18:27:34 +0100

    skill array with lenghts algoritms

---
+++ src/skills.js
@@ -0,0 +1,55 @@
+export const skills = [
+  {
+    id: 1,
+    skill: "1React"
+  },
+  {
+    id: 2,
+    skill: "2Redux"
+  },
+  {
+    id: 3,
+    skill: "3Redux-saga"
+  },
+  {
+    id: 4,
+    skill: "4Styled-components"
+  },
+  {
+    id: 5,
+    skill: "5JavaScript"
+  },
+  {
+    id: 6,
+    skill: "6React"
+  },
+  {
+    id: 7,
+    skill: "7 node.js"
+  },
+  {
+    id: 8,
+    skill: "8 praca zbiorowa"
+  },
+  {
+    id: 9,
+    skill: "9 homepage"
+  },
+  {
+    id: 10,
+    skill: "10 node.js"
+  },
+  {
+    id: 11,
+    skill: "11 praca zbiorowa"
+  },
+  {
+    id: 12,
+    skill: "12 homepage"
+  },
+];
+
+const skillLength = Object.keys(skills).length;
+export const firstColumn = Math.ceil(skillLength / 3) ;
+export const secondColumn = firstColumn + 1;
+export const thirdColumn = skillLength - (Math.floor(skillLength/3) - 1);
