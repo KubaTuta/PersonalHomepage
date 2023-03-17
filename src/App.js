c058e1706c0fc802ef533880cea8a250c421455f
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-17 10:49:00 +0100

    Theme Provider and global styles mounted

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import Skills from "./Components/Skills";
 import WantToLearn from "./Components/WantToLearn/index";
 import Portfolio from "./Components/Portfolio";
 import Contact from "./Components/Contact";
-import { Container } from "./Components/Common/Container";
-import Section from "./Components/Common/Section/index";
-
+import { Container } from "./styled";
+import Section from "./Common/Section/index";
+import { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
+import { selectDarkTheme } from "./Features/themeSlice";
+import { lightTheme, darkTheme } from "./themes";
+import { GlobalStyle } from "./GlobalStyles";
 
 function App() {
+  const isDarkTheme = useSelector(selectDarkTheme);
   return (
-    <Container>
-      <About />
-      <Section
-        header={"My skillset includes "}
-        body={<Skills />}
-      />
-      <Section
-        header={"What I want to learn"}
-        body={<WantToLearn />}
-      />
-      <Portfolio />
-      <Contact />
-    </Container>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Container>
+        <About />
+        <Section
+          header={"My skillset includes "}
+          body={<Skills />}
+        />
+        <Section
+          header={"What I want to learn"}
+          body={<WantToLearn />}
+        />
+        <Portfolio />
+        <Contact />
+      </Container>
+    </ThemeProvider>
   );
 }
 
