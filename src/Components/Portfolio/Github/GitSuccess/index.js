46786a229a44ec6f7a62d82a847338a9eb2cd64f
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-17 10:58:02 +0100

    adding links

--- src/Components/Portfolio/Github/GitSuccess/index.js
+++ src/Components/Portfolio/Github/GitSuccess/index.js
@@ -1,24 +1,23 @@
-import { ApiType, Block, H3 } from "./styled";
+import { ApiType, Block, H3, Link } from "./styled";
 
-const GitSuccess = ({repositories}) => {
+const GitSuccess = ({ repositories }) => {
   return (
-    repositories.map(({name, description, html_url, homepage}, index) => 
+    repositories.map(({ name, description, html_url, homepage }, index) =>
       <Block key={index}>
-       <ApiType>
-        <H3>{name}</H3>
-        </ApiType> 
+        <ApiType>
+          <H3>{name}</H3>
+        </ApiType>
         <ApiType>
           {description}
         </ApiType>
         <ApiType>
-          DEMO: {homepage}
+          DEMO: <Link href={homepage}>{homepage}</Link>
         </ApiType>
         <ApiType>
-          CODE: {html_url}
+          CODE: <Link href={html_url}>{html_url}</Link>
         </ApiType>
       </Block>
-     
-      )
+    )
   );
 }
 export default GitSuccess;
