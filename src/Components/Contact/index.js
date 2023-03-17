81023c392230a378cfb0c4875b38bad887fce46c
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-17 10:57:21 +0100

    repairing svg's

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,9 +1,9 @@
-import { Footer, Svg } from "../../styled";
-import { Description, Email, Heading } from "./styled";
-import github  from "../svg//git.svg";
-import facebook from "../svg/Facebook.svg";
-import instagram  from "../svg/Instagram.svg";
-import linked  from "../svg/Linked.svg";
+import { Footer } from "./styled";
+import { Description, Email, Heading, SvgLink } from "./styled";
+import { ReactComponent as Github } from "../svg//git.svg";
+import { ReactComponent as Facebook } from "../svg/Facebook.svg";
+import { ReactComponent as Instagram } from "../svg/Instagram.svg";
+import { ReactComponent as Linked } from "../svg/Linked.svg";
 
 const Contact = () => {
   return (
@@ -17,15 +17,14 @@ const Contact = () => {
       <Description>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique temporibus expedita voluptatibus distinctio tenetur debitis, itaque, voluptate mollitia libero eligendi placeat natus modi! Consequatur voluptatibus placeat dolor libero architecto quidem?
       </Description>
-      <p>
-        <Svg src={github} alt={"github"}></Svg>
-        <Svg src={facebook} alt={"facebook"}></Svg>
-        <Svg src={linked} alt={"linkedin"}></Svg>
-        <Svg src={instagram} alt={"instagram"}></Svg>
-      </p>
-      
+      <>
+        <SvgLink href="google.pl"><Github /></SvgLink>
+        <SvgLink href="google.pl"><Facebook /></SvgLink>
+        <SvgLink href="google.pl"><Instagram /></SvgLink>
+        <SvgLink href="google.pl"><Linked /></SvgLink>
+      </>
     </Footer>
-  
+
   )
 };
 
--- src/Components/Contact/styled.js
+++ src/Components/Contact/styled.js
@@ -1,16 +1,34 @@
 import styled from "styled-components";
 
+export const Footer = styled.div`
+`;
+
 export const Heading = styled.p`
   font-size: 12px;
-  color: ${({theme})=>theme.color.slateGray};
+  color: ${({theme})=>theme.color.smallText};
 `;
 
 export const Email = styled.p`
   font-size: 32px;
   font-weight: 900;
+  &:hover {
+    color: ${({theme})=>theme.color.scienceBlue};
+  }
 `;
 
 export const Description = styled.p`
   font-size: 18px;
   max-width: 670px;
+`;
+
+export const SvgLink = styled.a`
+  margin-right: 24px;
+  path {
+    fill: ${({theme})=>theme.color.icon};
+  }
+  &:hover {
+    path {
+      fill: ${({theme})=>theme.color.iconHover};
+    }
+  }
 `;
