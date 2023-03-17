79405f7ebda84938aa090dcbbc2750a2aa9d5a74
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-17 10:52:31 +0100

    move theme provider and global styles to app.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './GlobalStyles';
 import { Provider } from 'react-redux';
 import store from './Features/store';
-import { theme } from "./themes";
-import { ThemeProvider } from 'styled-components';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
         <App />
-      </ThemeProvider>
-
     </Provider>
   </React.StrictMode>
 );
