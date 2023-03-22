8d384f3da1a73f1f5375c3dfd7ae6f31450fc541
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-22 11:47:37 +0100

    margin top and bottom, pos relative

--- src/styled.js
+++ src/styled.js
@@ -1,20 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  margin: 0 auto;
+  margin: 50px auto;
   gap: 72px;
   max-width: 1216px;
   width: 100%;
-`;
-
-export const Wrapper = styled.div`
- background-color: ${({theme})=>theme.color.tile};
- border-radius: 4px;
- box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-`;
-
-export const ListWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  margin: 32px; 
+  position: relative;
 `;
