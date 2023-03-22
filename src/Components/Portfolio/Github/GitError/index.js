f4a5f4874b2b1a0daa13fb05b61150f508a88474
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-22 11:52:31 +0100

    error component

--- src/Components/Portfolio/Github/GitError/index.js
+++ src/Components/Portfolio/Github/GitError/index.js
@@ -1,6 +1,14 @@
+import { ReactComponent as Wrong } from "../../../svg/wrong.svg";
+import { Description, Wrapper } from "../../styled";
+import { Button, H3, Link } from "./styled";
+
 const GitError = () => (
-  <div>
-    error
-  </div>
+  <Wrapper>
+    <Wrong />
+    <H3>Ooops! Something went wrong...</H3>
+    <Description>Sorry, failed to load Github projects.</Description>
+    <Description>You can check them directly on Github.</Description>
+    <Link href="https://github.com/KubaTuta/" rel="noopener noreferrer"><Button>Go to Github</Button></Link>
+  </Wrapper>
 );
 export default GitError;
---
+++ src/Components/Portfolio/Github/GitError/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const H3 = styled.h3`
+  font-size: 24px;
+  margin-top: 24px;
+  margin-bottom: 24px;
+  color: ${({theme})=>theme.color.text};
+  @media(max-width: ${({theme})=>theme.breakpoint.mobile}) {
+    font-size: 16px;
+  };
+`;
+
+export const Button = styled.button`
+  font-size: 20px;
+  font-weight: 600;
+  border: none;
+  border-radius: 4px;
+  width: 168px;
+  height: 49px;
+  color: ${({theme})=>theme.color.tile};
+  background-color: ${({theme})=>theme.color.scienceBlue};
+  margin-top: 32px;
+  &:hover {
+    cursor: pointer;
+    outline: 4px solid ${({theme})=>theme.color.border};
+  }
+`;
+
+export const Link = styled.a`
+  text-decoration: none;
+`;
