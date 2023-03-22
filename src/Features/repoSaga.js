2ec2a57114e70faaccfa103a7389309c7ab02dea
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-22 11:54:00 +0100

    delay to be longer visible

--- src/Features/repoSaga.js
+++ src/Features/repoSaga.js
@@ -4,7 +4,7 @@ import { fetchRepositories, fetchRepositoriesError, fetchRepositoriesSuccess } f
 
 function* fetchRepositoriesHandler() {
   try {
-    yield delay(1000);
+    yield delay(5000);
     const repositories = yield call(getRepo);
     yield put(fetchRepositoriesSuccess(repositories));
   } catch (error) {
