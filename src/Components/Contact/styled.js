b4e65187f10a8eff7baa5fb5875cac58cec4885c
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-22 11:51:42 +0100

    styling mail and svg's

--- src/Components/Contact/styled.js
+++ src/Components/Contact/styled.js
@@ -6,22 +6,24 @@ export const Footer = styled.div`
 
 export const Heading = styled.p`
   font-size: 12px;
-  color: ${({theme})=>theme.color.smallText};
+  color: ${({ theme }) => theme.color.smallText};
   &:not(:last-child) {
     margin-bottom: 24px;
   }
 `;
 
-export const Email = styled.p`
+export const Email = styled.a`
+  color: ${({ theme }) => theme.color.text};
+  text-decoration: none;
   font-size: 32px;
   font-weight: 900;
   transition: 0.3s;
   width: fit-content;
   &:hover {
-    color: ${({theme})=>theme.color.scienceBlue};
+    color: ${({ theme }) => theme.color.scienceBlue};
     cursor: pointer;
   }
-  @media(max-width: ${({theme})=>theme.breakpoint.mobile}) {
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
     font-size: 18px;
     }
 `;
@@ -29,7 +31,7 @@ export const Email = styled.p`
 export const Description = styled.p`
   font-size: 18px;
   max-width: 670px;
-  @media(max-width: ${({theme})=>theme.breakpoint.mobile}) {
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
     font-size: 14px;
     }
 `;
@@ -37,18 +39,17 @@ export const Description = styled.p`
 export const SvgLink = styled.a`
   margin-right: 24px;
   path {
-    fill: ${({theme})=>theme.color.icon};
+    fill: ${({ theme }) => theme.color.icon};
     transition: 0.3s;
   }
   &:hover {
     path {
-      fill: ${({theme})=>theme.color.iconHover};
+      fill: ${({ theme }) => theme.color.iconHover};
     }
   }
-  @media(max-width: ${({theme})=>theme.breakpoint.mobile}) {
-    path {
-      fill: red;
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    svg {
       width: 32px;
     }
-    }
+  }
 `;
