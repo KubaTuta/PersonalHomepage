42963bf7acc6c690a16678764ef8fa549efaac4a
Kuba Tuta <tuta.jakub@gmail.com>
2023-05-15 10:48:55 +0200

    ts convertion

---
+++ src/skills.tsx
@@ -0,0 +1,99 @@
+export const skills = [
+  {
+    id: 1,
+    skill: "HTML"
+  },
+  {
+    id: 2,
+    skill: "CSS"
+  },
+  {
+    id: 3,
+    skill: "JavaScript"
+  },
+  {
+    id: 4,
+    skill: "React"
+  },
+  {
+    id: 5,
+    skill: "Styled Components"
+  },
+  {
+    id: 6,
+    skill: "Redux"
+  },
+  {
+    id: 7,
+    skill: "Redux-saga"
+  },
+  {
+    id: 8,
+    skill: "Redux-toolkit"
+  },
+  {
+    id: 9,
+    skill: "Github"
+  },
+  {
+    id: 10,
+    skill: "Teamwork"
+  },
+  {
+    id: 11,
+    skill: "Fetch"
+  },
+  {
+    id: 12,
+    skill: "Async/Await"
+  },
+  {
+    id: 13,
+    skill: "Axios"
+  },
+  {
+    id: 14,
+    skill: "React Router"
+  },
+];
+
+export const skillsToLearn = [
+  {
+    id: 1,
+    skill: "Typescript"
+  },
+  {
+    id: 2,
+    skill: "Python"
+  },
+  {
+    id: 3,
+    skill: "React Native"
+  },
+  {
+    id: 4,
+    skill: "SQL"
+  },
+  {
+    id: 5,
+    skill: "Material-UI"
+  },
+  {
+    id: 6,
+    skill: "More and more..."
+  },
+];
+
+// column logic:
+
+const skillLength = Object.keys(skills).length;
+export const firstColumn = Math.ceil(skillLength / 3) ;
+export const secondColumn = firstColumn + 1;
+export const thirdColumn = skillLength - (Math.floor(skillLength/3) - 1);
+export const firstOfTwo = Math.ceil(skillLength / 2) ;
+
+const skillsLengthToLearn = Object.keys(skillsToLearn).length ;
+export const firstColumnToLearn = Math.ceil(skillsLengthToLearn / 3) ;
+export const secondColumnToLearn = firstColumnToLearn + 1;
+export const thirdColumnToLearn = skillsLengthToLearn - (Math.floor(skillsLengthToLearn/3) - 1);
+export const firstOfTwoToLearn = Math.ceil(skillsLengthToLearn / 2) ;
---
+++ src/styled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 50px auto;
+  gap: 72px;
+  max-width: 1216px;
+  width: 100%;
+  position: relative;
+`;
