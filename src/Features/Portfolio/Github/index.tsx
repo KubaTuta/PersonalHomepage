d2f826934f46e10c38e0449d763a16423c709320
Kuba Tuta <tuta.jakub@gmail.com>
2023-05-16 15:39:04 +0200

    preventing null

--- src/Features/Portfolio/Github/index.tsx
+++ src/Features/Portfolio/Github/index.tsx
@@ -6,10 +6,23 @@ import GitError from "./GitError";
 import GitLoading from "./GitLoading";
 import GitSucceess from "./GitSuccess";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+  homepage: string;
+}
+
+interface Repositories {
+  repositories: Repository[];
+}
+
+
+
 const Github = () => {
   const dispatch = useDispatch();
 
-  const repositories = useSelector(selectRepos);
+  const repositories = useSelector(selectRepos) ?? [];
   const repoStatus = useSelector(selectReposStatus);
 
   useEffect(() => { dispatch(fetchRepositories()) }, [dispatch]);
