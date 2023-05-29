5d23d2e28fe5eb0c954b4a91a21f7ebd644d5936
Kuba Tuta <tuta.jakub@gmail.com>
2023-05-29 21:45:44 +0200

    store update minus saga

--- src/Features/store.tsx
+++ src/Features/store.tsx
@@ -1,21 +1,13 @@
-import createSagaMiddleware from "@redux-saga/core";
-import { configureStore } from "@reduxjs/toolkit";
-import repoSlice from "./repoSlice";
-import saga from "./saga";
-import themeSlice from "./themeSlice";
 
-const sagaMiddleware = createSagaMiddleware();
+import { configureStore } from "@reduxjs/toolkit";
+import themeSlice from "./themeSlice";
 
 const store = configureStore({
   reducer: {
-    repositories: repoSlice,
     theme: themeSlice,
-  },
-  middleware: [sagaMiddleware],
+  }
 });
 
 export type Rootstate = ReturnType<typeof store.getState>;
-
-sagaMiddleware.run(saga);
 
 export default store;
